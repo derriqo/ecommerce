53844a49c3b6efc67e7637a397885fb94de9a185
derriqo <williamderrick100@gmail.com>
2020-05-06 15:33:15 +0300

    Completed store and cart page.

--- store/views.py
+++ store/views.py
@@ -4,16 +4,15 @@ from django.shortcuts import render
 
 
 def store(request):
-    contex = {}
-    return render(request, 'store.html', contex)
+    context = {}
+    return render(request, 'store.html', context)
 
 
 def cart(request):
-    contex = {}
-    return render(request, 'cart.html', contex)
+    context = {}
+    return render(request, 'cart.html', context)
 
 
 def checkout(request):
-    contex = {}
-    return render(request, 'checkout.html', contex)
-
+    context = {}
+    return render(request, 'checkout.html', context)
