342790ea59f211f59b90cf1af0e41a36130e8457
derriqo <williamderrick100@gmail.com>
2020-05-14 09:11:09 +0300

    migrations

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce',
+        'USER': 'derrick',
+        'PASSWORD': 'qwerty123',
+
     }
 }
 
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
