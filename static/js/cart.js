0942299d0a0a9d89d6b48dc6fb57bb60c15e6cc9
derriqo <williamderrick100@gmail.com>
2020-05-22 11:26:04 +0300

    Updates

--- static/js/cart.js
+++ static/js/cart.js
@@ -7,5 +7,12 @@ for (var i = 0; i < updateBtns.length; i++) {
         var action = this.dataset.action
         console.log('productId:', productId, 'action:', action)
 
+        console.log('USER:,user')
+        if (user == 'AnonymusUser') {
+            console.log('User is not authenticated')
+        } else {
+            console.log('User is authenticated, sending data...')
+        }
+
     })
 }
