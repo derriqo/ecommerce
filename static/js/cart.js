bee36b79fd7b2f9d1c370ccf6649c017f6fa0a4c
derriqo <williamderrick100@gmail.com>
2020-05-25 14:21:03 +0300

    Updates.

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,18 +1,38 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName("update-cart");
 
 for (var i = 0; i < updateBtns.length; i++) {
+  updateBtns[i].addEventListener("click", function () {
+    var productId = this.dataset.product;
+    var action = this.dataset.action;
+    console.log("productId:", productId, "action:", action);
 
-    updateBtns[i].addEventListener('click', function () {
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('productId:', productId, 'action:', action)
+    console.log("USER:", user);
 
-        console.log('USER:,user')
-        if (user == 'AnonymusUser') {
-            console.log('User is not authenticated')
-        } else {
-            console.log('User is authenticated, sending data...')
-        }
+    if (user === "AnonymousUser") {
+      console.log("Not logged in");
+    } else {
+      updateUserOrder(productId, action);
+    }
+  });
+}
 
+function updateUserOrder(productId, action) {
+  console.log("User is authenticated, sending data...");
+
+  var url = "/update_item/";
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      Content_Type: "application/json",
+    },
+    body: JSON.stringify({ productId: productId, action: action }),
+  })
+    .then((response) => {
+      return response.json();
     })
+
+    .then((data) => {
+      console.log("data:", data);
+    });
 }
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
 # Create your views here.
 
 
@@ -38,3 +39,7 @@ def checkout(request):
 
         context = {'items': items, 'order': order}
     return render(request, 'checkout.html', context)
+
+
+def updateItem(request):
+    return JsonResponse("Item was added", safe=False)
